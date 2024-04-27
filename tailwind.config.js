dce35be66c1aa78f5f8cd020a24a59ab3099d6a4
Tim Quigg <timjquigg@gmail.com>
2024-04-27 11:16:22 -0600

    Updated styles for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       center: true,
       // padding: "2rem",
     },
+    minWidth: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
     fontFamily: {
       noto: ['"Noto Sans Display", sans-serif'],
       dosis: ['"Dosis", sans-serif'],
