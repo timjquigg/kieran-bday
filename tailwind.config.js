8117aeedff60fb8735e9953bf33142897c71bc45
Tim Quigg <timjquigg@gmail.com>
2024-04-27 21:44:22 -0600

    Combined venue & details

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
       "3/4": "75%",
       full: "100%",
     },
+    maxWidth: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
     fontFamily: {
       noto: ['"Noto Sans Display", sans-serif'],
       dosis: ['"Dosis", sans-serif'],
